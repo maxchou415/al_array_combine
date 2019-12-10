b23e4a7e757025cd6d788b94322d466c44ac70f9
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-12-11 01:56:43 +0800

    - done

---
+++ index.js
@@ -0,0 +1,27 @@
+const rawArray = [{
+  houseId: 123,
+  src: '123'
+}, {
+  houseId: 123,
+  src: '456'
+}, {
+  houseId: 456,
+  src: '769'
+}, {
+  houseId: 456,
+  src: '678'
+}]
+
+let indexArray = []
+
+// Create a array with basic data (houseId) on related index.
+rawArray.forEach(i => {
+  indexArray[i.houseId] = { houseId: i.houseId, src: [] }
+})
+
+// Push data into targeted houseId
+rawArray.forEach(i => {
+  indexArray[i.houseId].src.push(i.src)
+})
+
+console.log(indexArray)
